06304c8a37f12c86c5603709aaba8fd98b14a8f2
sanjay midha <designbysanjaymidha@gmail.com>
2025-10-01 19:11:57 +0530

    client changes

--- public/js/main.js
+++ public/js/main.js
@@ -162,3 +162,17 @@ window.addEventListener('resize', function () {
 });
 
 
+// Scroll to section on click
+document.querySelectorAll('nav a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener("click", function (e) {
+      e.preventDefault();
+      const target = document.querySelector(this.getAttribute("href"));
+      if (target) {
+        target.scrollIntoView({
+          behavior: "smooth",
+          block: "start"
+        });
+      }
+    });
+  });
+
